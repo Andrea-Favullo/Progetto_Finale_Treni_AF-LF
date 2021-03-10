15f5dcc5fd1e46638dd008622231bb156f9b4a4f
Andrea-Favullo <favullo.andrea.dennis@iisgalvanimi.edu.it>
2021-03-10 12:18:22 +0000

    demo api

---
+++ client/src/app/trenitalia-prova.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { TrenitaliaProvaService } from './trenitalia-prova.service';
+
+describe('TrenitaliaProvaService', () => {
+  let service: TrenitaliaProvaService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(TrenitaliaProvaService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
