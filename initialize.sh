b9b44f8c7fe1164a93ec1bea2b8c3322bf212b06
Andrea-Favullo <55271746+Andrea-Favullo@users.noreply.github.com>
2021-03-17 17:57:47 +0100

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,11 +1,11 @@
 cd /workspace/Progetto_Finale_Treni_AF-LF ;
 npm i -g @angular/cli ;
 cd client/ ;
-npm i ;
 npm i cors ;
 npm i angular-google-charts ;
-cd ../server ;
 npm i ;
+cd ../server ;
 npm i cors ;
 npm i mongodb ;
 npm i express
+npm i ;
