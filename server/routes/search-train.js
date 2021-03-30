21024b75665156eba4ccf276e648fefb17de00f0
FoxLuc <lucaf8502@gmail.com>
2021-03-30 10:07:33 +0000

    demo: avanti con la GUI + test richieste API

--- server/routes/search-train.js
+++ server/routes/search-train.js
@@ -5,11 +5,11 @@ var moment = require('moment');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-
     res.setHeader('Access-Control-Allow-Origin', '*');
 
     (async () => {
         const t = new Trenitalia();
+
         const stations_from = await t.autocomplete("milano");
         const station_from = stations_from[0].name;
         const stations_to = await t.autocomplete("bari");
@@ -17,8 +17,14 @@ router.get('/', function (req, res, next) {
 
         const date = moment().format("DD/MM/YYYY");
         const solutions = await t.getOneWaySolutions(station_from, station_to, date, "13", 2, 0);
-        console.log(solutions)
-        res.send(solutions);
+        
+        id_solution = solutions[0]["idsolution"]
+        console.log(id_solution)
+        //test login
+        solution = t.getSolutionInfo(id_solution);
+        
+        console.log(solution)
+        res.send(solution);
     })();
 });
 
