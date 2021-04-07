6754246fed469e02258ce3e678fbfe85f3b26436
Andrea-Favullo <favullo.andrea.dennis@iisgalvanimi.edu.it>
2021-04-07 12:06:45 +0000

    aggiunte query mongodb per search-station

--- server/routes/search-station.js
+++ server/routes/search-station.js
@@ -3,10 +3,21 @@ var router = express.Router();
 var Trenitalia = require("api-trenitalia");
 var moment = require('moment');
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = 'mongodb+srv://andrea-favullo:m0ng0D4RI0B4nF1@clustersdg16.dnkc2.mongodb.net/ClusterSDG16?retryWrites=true&w=majority';
+
 router.get('/', function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    //richiesta mongodb
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const collection = client.db("TreniDB").collection("dati_geospaziali");
+        collection.find({}).toArray((err, result) => {
+            if (err) console.log(err.message);
+            else { res.send(result); console.log(result); }
+            client.close();
+        });
+    });
 });
 
 router.get('/name/:stationName', function (req, res, next) {
@@ -14,7 +25,15 @@ router.get('/name/:stationName', function (req, res, next) {
 
     station_name = req.params.stationName;
 
-    //richiesta mongodb
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const collection = client.db("TreniDB").collection("dati_geospaziali");
+        collection.find({"name" : {"$regex" : `.*${station_name}.*`}}).toArray((err, result) => {
+            if (err) console.log(err.message);
+            else { res.send(result); console.log(result); }
+            client.close();
+        });
+    });
 });
 
 module.exports = router;
