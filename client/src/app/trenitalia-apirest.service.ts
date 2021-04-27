23a480c711b51c4f6850d871a3d4a2d4a55e4d90
FoxLuc <lucaf8502@gmail.com>
2021-04-27 09:39:44 +0000

    client salvataggio

--- client/src/app/trenitalia-apirest.service.ts
+++ client/src/app/trenitalia-apirest.service.ts
@@ -5,33 +5,33 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class TrenitaliaProvaService {
-  serverURL="https://3000-blue-crayfish-59k37b8a.ws-eu03.gitpod.io";
+  serverURL="https://3000-pink-lynx-62s0d1so.ws-eu03.gitpod.io/";
 
   constructor(private http: HttpClient) { }
 
   ricercaEsempio(){
-    const url = `${this.serverURL}/search-train/`
+    const url = `${this.serverURL}search-train/`
     let obs = this.http.get(url);
     console.log(obs);
     return obs;
   }
 
   ricercaPartenzaArrivo(origin : string, destination : string){
-    const url = `${this.serverURL}/search-train/from-to/${origin}/${destination}`
+    const url = `${this.serverURL}search-train/from-to/${origin}/${destination}`
     let obs = this.http.get(url);
     console.log(obs);
     return obs;
   }
 
   ricercaNomeStazione(name: string){
-    const url = `${this.serverURL}/search-station/name/${name}`
+    const url = `${this.serverURL}search-station/name/${name}`
     let obs = this.http.get(url);
     console.log(obs);
     return obs;
   }
 
   ricercaIdTreno(id: string){
-    const url = `${this.serverURL}/search-train/train-id/${id}`
+    const url = `${this.serverURL}search-train/train-id/${id}`
     let obs = this.http.get(url);
     console.log(obs);
     return obs;
