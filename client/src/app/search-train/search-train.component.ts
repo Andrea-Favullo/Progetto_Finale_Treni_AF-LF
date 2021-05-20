e985a5820af0ae80026c2d32898c3d3f67f8888e
Andrea-Favullo <favullo.andrea.dennis@iisgalvanimi.edu.it>
2021-05-20 14:52:03 +0000

    release 1.0 revamp grafica

--- client/src/app/search-train/search-train.component.ts
+++ client/src/app/search-train/search-train.component.ts
@@ -14,11 +14,11 @@ export class SearchTrainComponent implements OnInit {
   tiporichiesta: string = "NIENTE";
   loading: boolean = false;
 
-  constructor(public trenitalia: TrenitaliaProvaService){}
+  constructor(public trenitalia: TrenitaliaProvaService) { }
 
   ngOnInit(): void { }
 
-  isLoading(): boolean{
+  isLoading(): boolean {
     return this.loading;
   }
 
@@ -32,14 +32,14 @@ export class SearchTrainComponent implements OnInit {
     console.log(`non sta caricando`);
   }
 
-  ConvOrario(ms : any): any {
+  ConvOrario(ms: any): any {
     var date = new Date(ms);
     return date;
   }
 
-  capitalizeWords(text: string): string{
-    text=text.toLowerCase();
-    return text.replace(/(?:^|\s)\S/g, (res)=>{
+  capitalizeWords(text: string): string {
+    text = text.toLowerCase();
+    return text.replace(/(?:^|\s)\S/g, (res) => {
       return res.toUpperCase();
     })
   };
@@ -59,13 +59,19 @@ export class SearchTrainComponent implements OnInit {
   richiestaPartenzaDestinazione(partenza: HTMLInputElement, destinazione: HTMLInputElement): void {
     this.tiporichiesta = "STANDBY";
     this.onRequestStarted();
-    this.obs = this.trenitalia.ricercaPartenzaArrivo(partenza.value, destinazione.value);
-    this.obs.subscribe((data) => {
-      this.dati = data;
-      console.log(this.dati);
-      this.tiporichiesta = "PARTENZA_DEST";
-      this.onRequestFinished();
-    });
+    if (partenza.value.replace(" ", "") == "" || destinazione.value.replace(" ", "") == "") {
+      this.richiesta();
+    } else {
+
+      this.obs = this.trenitalia.ricercaPartenzaArrivo(partenza.value, destinazione.value);
+      this.obs.subscribe((data) => {
+        this.dati = data;
+        console.log(this.dati);
+        this.tiporichiesta = "PARTENZA_DEST";
+        this.onRequestFinished();
+      });
+
+    }
   }
 
   richiestaIdTreno(id: HTMLInputElement): void {
@@ -73,9 +79,12 @@ export class SearchTrainComponent implements OnInit {
     this.dati = [];
     this.onRequestStarted();
     let id_value = id.value;
+    if (id_value.replace(" ", "") == "") {
+      id_value="10581"
+    }
     this.obs = this.trenitalia.ricercaIdTreno(id_value);
-    this.obs.subscribe((data : any) => {
-      if(data['exists']){
+    this.obs.subscribe((data: any) => {
+      if (data['exists']) {
         this.dati = data;
         console.log(this.dati);
         this.tiporichiesta = "ID_TRENO";
@@ -86,27 +95,28 @@ export class SearchTrainComponent implements OnInit {
       }
       this.onRequestFinished();
     });
+
   }
-  toText(duration: string): string{
+  toText(duration: string): string {
 
     let splitted = duration.split(":");
     let result = "";
     let ore = splitted[0];
     let minuti = splitted[1];
 
-    if(ore != "00"){
+    if (ore != "00") {
 
-      if (ore.charAt(0)=="0"){
+      if (ore.charAt(0) == "0") {
         ore = ore.charAt(1)
       }
       result = result + ore + " ore "
 
-      if (minuti.charAt(0)=="0"){
+      if (minuti.charAt(0) == "0") {
         minuti = minuti.charAt(1)
       }
       result = result + minuti + " minuti "
-    }else{
-      if (minuti.charAt(0)=="0"){
+    } else {
+      if (minuti.charAt(0) == "0") {
         minuti = minuti.charAt(1)
       }
       result = result + minuti + " minuti "
--- server/routes/search-train.js
+++ server/routes/search-train.js
@@ -76,6 +76,99 @@ router.get('/from-to/:DepartureStation/:ArrivalStation', function (req, res, nex
 });
 
 //ricerca treno in base all'ID del treno
+router.get('/train-id/', function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    
+    //recupero ID del treno
+    const train_id = "10581";
+    console.log("ID da ricercare: \"" + train_id + "\"")
+    //recupero URL a cui applicare il Web Scraping
+    const train_url = `https://www.trenord.it/rest/render/train-sub-detail?trainId=${train_id}`;
+    console.log("URL web scraping: \"" + train_url + "\"")
+
+    request(train_url, function (error, response, body) {
+        //print di stato della connessione
+        console.error('Errore:', error);
+        console.log('Codice di Stato:', response && response.statusCode);
+
+        //recupero il body della pagina HTML e lo trasformo in un DOM
+        body_from_JSON = JSON.parse(body)['message']
+        const frag = JSDOM.fragment(body_from_JSON);
+
+        //recupero quindi tutti i paragrafi
+        var node_list = frag.querySelectorAll("p")
+
+        var staz_inter = frag.querySelectorAll("li")
+        
+        let stazioni_intermedie = []
+
+        for(let i=0; i<staz_inter.length; i++){
+
+            let stazione = staz_inter.item(i).getElementsByTagName("p")
+            
+            //nome stazione
+            let nome_stazione = stazione.item(0).textContent
+            //arrivo alla stazione
+            let arrival_time = new String(new String(stazione.item(1).textContent).toString().replace("\n", "").trim()).split("\n")[1].trim().toString()
+            
+            let secondo_campo = ""
+            if (i==0){
+                secondo_campo="departure_time"
+            }else{
+                secondo_campo="arrival_time"
+            }
+            stazioni_intermedie.push( JSON.parse( `{ \"isFirst\":${i==0},\n\"isLast\":${i==staz_inter.length-1},\n\"station_name\":\"${nome_stazione}\",\n\"${secondo_campo}\":\"${arrival_time}\"\n}` ) )
+            
+        }
+
+        si=""
+
+        for(let i=0; i<stazioni_intermedie.length; i++){
+            let stazione = stazioni_intermedie[i]
+            if (!stazione.isLast){
+                si += JSON.stringify(stazione) + ",\n"
+            }else{
+                si += JSON.stringify(stazione)
+            }
+        }
+
+        //metodi pulizia stringhe
+        /**mette la prima lettera di una stringa in maiuscolo
+         * @param {* string stringa da elaborare} string 
+         */
+        function upperCaseFirstLetter(string) {
+            return string.charAt(0).toUpperCase() + string.slice(1);
+        }
+        /**tutte le lettere di una stringa vengono trasformate in minuscolo
+         * @param {* string stringa da elaborare} string 
+         */
+        function lowerCaseAllWordsExceptFirstLetters(string) {
+            return string.replace(/\S*/g, function (word) {
+                return word.charAt(0) + word.slice(1).toLowerCase();
+            });
+        }
+
+        //elaborazione stazione partenza
+        var stazione_partenza = node_list.item(2).textContent
+        stazione_partenza = lowerCaseAllWordsExceptFirstLetters(stazione_partenza)
+        stazione_partenza = upperCaseFirstLetter(stazione_partenza)
+
+        //elaborazione stazione destinazione
+        var stazione_destinazione = node_list.item(4).textContent
+        stazione_destinazione = lowerCaseAllWordsExceptFirstLetters(stazione_destinazione)
+        stazione_destinazione = upperCaseFirstLetter(stazione_destinazione)
+
+        //recupero l'url dell'immagine del tipo del treno
+        var img_url = frag.querySelector("img").src
+        
+        //elaboro la risposta e la consegno
+        response = `{ \n\t\"exists\":${stazione_partenza!=""},\n\t\"train_id\":\"${train_id}\", \n\t\"departure_st\":\"${stazione_partenza}\", \n\t\"arrival_st\":\"${stazione_destinazione}\", \n\t\"img_url\":\"${img_url}\", \"middle_stations\":[${si}] \n}`
+        console.log("Contenuti elaborati:\n"+response)
+        res.send( JSON.parse(response) );
+    });
+});
+
+//ricerca treno in base all'ID del treno
 router.get('/train-id/:trainID', function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     
